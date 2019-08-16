bdf38766ab69d92a91d90f9e3fd9142591ddaced
Jose Angel de Bustos Perez <abustosp@redhat.com>
2019-08-16 22:33:49 +0200

    ceph python scripts

---
+++ hands-on-lab-script/ceph/scripts/list_buckets.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+# (c) 2018 Jose Angel de Bustos Perez <jadebustos@redhat.com>
+# Distributed under GPLv3 License (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+import boto
+import boto.s3.connection
+
+# this scripts shows the buckets in the s3 endpoint
+
+def main():
+  # put here your access_key and secret_key to access s3 bucket
+  #access_key = 'GEIICT90BB597P57B9BB'
+  #secret_key = 'pZlzuKquhrBzQ74mXkAOIjpSdzoWRZwqKQzzRQIq'
+  access_key = 'foo'
+  secret_key = 'bar'
+
+  # your rados host
+  radoshost = 'ceph-nano-services-test-ceph.apps.cluster-2956.sandbox478.opentlc.com'
+
+  # your rados port
+  radosport = 8080
+
+  conn = boto.connect_s3(
+    aws_access_key_id = access_key,
+    aws_secret_access_key = secret_key,
+    host = radoshost,
+    port = radosport,
+    is_secure=False,
+    calling_format = boto.s3.connection.OrdinaryCallingFormat()
+    )
+
+  for bucket in conn.get_all_buckets():
+    print "{name}\t{created}".format(
+      name = bucket.name,
+      created = bucket.creation_date,
+      )
+
+if __name__ == "__main__":
+  main()
