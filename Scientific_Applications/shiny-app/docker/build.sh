6fe195620af4c8a43efae349154643a993a2c133
Jose Antonio Gonzalez Prada <josgonza@redhat.com>
2019-08-20 13:18:15 +0200

    shiny-app: fixing access permissions for shiny user

--- Scientific_Applications/shiny-app/docker/build.sh
+++ Scientific_Applications/shiny-app/docker/build.sh
@@ -14,10 +14,12 @@ HTTP_PROXY=${HTTP_PROXY:-$3}
 
 BUILD_ARG=""
 
-if [ ! -z ${HTTP_PROXY} ]; then
-  BUILD_ARG="${BUILD_ARG} --build-arg http_proxy=${HTTP_PROXY}"
+if [ ! -z "${HTTP_PROXY}" ]; then
+  BUILD_ARG="--build-arg http_proxy=${HTTP_PROXY}"
   BUILD_ARG="${BUILD_ARG} --build-arg https_proxy=${HTTP_PROXY}"
   BUILD_ARG="${BUILD_ARG} --build-arg no_proxy=180.14.131.80,.bsch,.cluster.local,.corp,.local,.novalocal"
+
+  echo "BUILD_ARG=${BUILD_ARG}"
 fi
 
 echo "Building backup docker image .."
