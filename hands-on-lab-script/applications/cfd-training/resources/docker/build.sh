8727e242ae57645be8f835f79b09c592d1cd22c9
Raul Sevilla Canavate <rsevilla@redhat.com>
2019-09-05 12:33:53 +0200

    cfd Notebook now able to run as non-root user

--- hands-on-lab-script/applications/cfd-training/resources/docker/build.sh
+++ hands-on-lab-script/applications/cfd-training/resources/docker/build.sh
@@ -6,5 +6,4 @@ BUILD_ARG="ubuntu:bionic-20190612@sha256:9b1702dcfe32c873a770a32cfd306dd7fc1c4fd
 
 echo "Building backup docker image .."
 
-#docker build --no-cache=true --build-arg BASE_CONTAINER=${BUILD_ARG} -t ${IMAGE_NAME}:${IMAGE_TAG} --network host -f ./docker/Dockerfile ./docker
-docker build --no-cache=true -t ${IMAGE_NAME}:${IMAGE_TAG} --network host -f ./docker/Dockerfile ./docker
+docker build --no-cache=true -t ${IMAGE_NAME}:${IMAGE_TAG} .
--- hands-on-lab-script/applications/cfd-training/resources/docker/jupyter_notebook_config.py
+++ hands-on-lab-script/applications/cfd-training/resources/docker/jupyter_notebook_config.py
@@ -9,7 +9,7 @@ import stat
 
 c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
+c.NotebookApp.port = 8080
 c.NotebookApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
